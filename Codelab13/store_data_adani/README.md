b464d501944e0768c3ef6d4ff77a242608434add
adanisalsabila <adanisalsabilaaa@gmail.com>
2025-12-16 00:28:33 +0700

    codelab13-p2

--- Codelab13/store_data_adani/README.md
+++ Codelab13/store_data_adani/README.md
@@ -1,7 +1,13 @@
 # store_data_adani
 
-# PRAKTIKUM 1 : Konversi Dart model ke JSON
+# PRAKTIKUM 1 : Konversi Dart model ke JSON    
 
 ![hasil](img/praktikum1.png)      
 
 ![hasil](img/praktikum1b.png)       
+
+# PRAKTIKUM 2 : Handle kompatibilitas data JSON      
+
+![hasil](img/praktikum2.png)      
+
+
--- Codelab13/store_data_adani/lib/pizza.dart
+++ Codelab13/store_data_adani/lib/pizza.dart
@@ -14,14 +14,33 @@ class Pizza {
     required this.imageUrl,
   });
 
-  // Langkah 13: Constructor fromJson (Map -> Object)
-  factory Pizza.fromJson(Map<String, dynamic> json) {
+//   // Langkah 13: Constructor fromJson (Map -> Object)
+//   factory Pizza.fromJson(Map<String, dynamic> json) {
+//     return Pizza(
+//       id: json['id'],
+//       pizzaName: json['pizzaName'],
+//       description: json['description'],
+//       price: json['price'],
+//       imageUrl: json['imageUrl'],
+//     );
+//   }
+// }
+
+factory Pizza.fromJson(Map<String, dynamic> json) {
     return Pizza(
-      id: json['id'],
-      pizzaName: json['pizzaName'],
-      description: json['description'],
-      price: json['price'],
-      imageUrl: json['imageUrl'],
+      // Langkah 3: Handle ID (String/Int -> Int)
+      // Mengambil value, ubah ke string, coba parse ke int. Jika gagal/null, beri nilai 0.
+      id: int.tryParse(json['id'].toString()) ?? 0,
+
+      // Langkah 5, 6, 10: Handle String & Null
+      // Cek apakah key ada? Jika null, beri nilai default 'Unknown ...'.
+      // toString() memastikan jika data dikirim sebagai angka, tetap terbaca sebagai string.
+      pizzaName: json['pizzaName']?.toString() ?? 'Unknown Pizza',
+      description: json['description']?.toString() ?? 'No description available',
+      imageUrl: json['imageUrl']?.toString() ?? 'images/default.png', // Beri gambar default jika null
+
+      // Langkah 7 & 8: Handle Price (String/Double/Int -> Double)
+      // Ubah ke string dulu, lalu parse ke double.
+      price: double.tryParse(json['price'].toString()) ?? 0.0,
     );
-  }
-}
+  } } 
